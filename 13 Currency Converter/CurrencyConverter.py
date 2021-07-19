5d999b407fdb276678c16b27172c11f99bcec553
Alok Kumar Maurya <ry7069577@gmail.com>
2021-07-18 22:51:38 -0700

    Update CurrencyConverter.py

--- 13 Currency Converter/CurrencyConverter.py
+++ 13 Currency Converter/CurrencyConverter.py
@@ -37,7 +37,7 @@ if __name__ =='__main__':
     currency_field = tk.Entry(main_window,justify="right",textvariable=input_text)
     currency_field.grid(row=2,padx=5,pady=5)
 
-    country_code = ["INR","USD","CAD","CNY","DKK","EUR"]
+    country_code = ["AED","AFN","ALL","AMD","ANG","AOA","ARS","AUD","AWG","AZN","BAM","BBD","BDT","BGN","BHD","BIF","BMD","BND","BOB","BRL","BSD","BTC","BTN","BWP","BYN","BYR","BZD","CAD","CDF","CHF","CLF","CLP","CNY","COP","CRC","CUC","CUP","CVE","CZK","DJF","DKK","DOP","DZD","EGP","ERN","ETB","CUR","FJD","FKP","GBP","GEL","GGP","GHS","GIP","GMD","GNF","GTQ","GYD","HKD","HNL","HRK","HTG","HUF","IDR","ILS","IMP","INR","IQD","IRR","ISK","JEP","JMD","JOD","JPY","KES","KGS","KHR","KMF","KPW","KRW","KWD","KYD","KZT","LAK","LBP","LKR","LDR","LSL","LTL","LVL","LYD","MAD","MDL","MGA","MKD","MMK","MNT","MOP","MRO","MUR","MVR","MWK","MXN","MYR","MZN","NAD","NGN","NIO","NOK","NPR","NZD","OMR","PAB","PEN","PGK","PHP","PKR","PLN","PYG","QAR","RON","RSD","RUB","RWF","SAR","SBD","SCR","SDG","SEK","SGD","SHP","SLL","SOS","SRD","STD","SVC","SYP","SZL","THB","TJS","TMT","TND","TOP","TRY","TID","TWD","TZS","UAH","UGX","USD","UYU","UZS","VEF","VND","VUV","WST","XAF","XAG","XAU","XCD","XDR","XOF","XPF","YER","ZAR","ZMK","ZMW","ZWL"]
     source_value = tk.StringVar()
 
     source_value_selection = ttk.Combobox(main_window,values=country_code,textvariable=source_value)
